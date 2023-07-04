5e427190ffae086dfc2426bcf279e83534abf0f1
Yesica-MP <yesica.lmp@gmail.com>
2023-07-04 13:15:56 -0600

    Week14

--- src/main/java/pet/store/entity/PetStore.java
+++ src/main/java/pet/store/entity/PetStore.java
@@ -25,19 +25,23 @@ public class PetStore {
 	
 	private String petStoreName;
 	private String petStoreAddress;
-	private String city;
-	private String state;
-	private String zip;
-	private String phone;
+	private String petStoreCity;
+	private String petStoreState;
+	private String petStoreZip;
+	private String petStorePhone;
 	
-	@EqualsAndHashCode.Exclude
-	@ToString.Exclude
 	@ManyToMany(cascade = CascadeType.PERSIST)
 	@JoinTable(name = "pet_store_customer", joinColumns = @JoinColumn(name = "pet_store_id"), inverseJoinColumns = @JoinColumn(name = "customer_id"))
-	private Set<Customer> customers = new HashSet<>();
-	
 	@EqualsAndHashCode.Exclude
 	@ToString.Exclude
+	private Set<Customer> customers = new HashSet<>();
+	
+	
 	@OneToMany(mappedBy = "petStore", cascade = CascadeType.ALL, orphanRemoval = true)
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
 	private Set<Employee> employees = new HashSet<>();
+
+
+	
 }
